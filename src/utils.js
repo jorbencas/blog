05cd994d36bbf8e39484149102ad52a7cd323178
jorbencas <jorbencas@gmail.com>
2023-01-28 16:51:45 +0100

    function doSomething

--- src/utils.js
+++ src/utils.js
@@ -2,26 +2,32 @@ import mounths from "./layouts/months.json";
 import days from "./layouts/days.json";
 
 export const formatDatePost = (date) => {
-    let fullDate = new Date(date);
-    let day = days[fullDate.getDay()];
-    let month = mounths[fullDate.getMonth()];
-    let year = fullDate.getFullYear();
-    let numday = fullDate.getDate();
-    return day + ", " + numday + " de " + month + " del " + year;
-  };
+  let fullDate = new Date(date);
+  let day = days[fullDate.getDay()];
+  let month = mounths[fullDate.getMonth()];
+  let year = fullDate.getFullYear();
+  let numday = fullDate.getDate();
+  return day + ", " + numday + " de " + month + " del " + year;
+};
 
 export const getSortedPosts = (allPosts) => {
   return allPosts
-  .filter((post) => !post.frontmatter.draft)
-  .sort(
-    (a, b) =>
-      new Date(b.frontmatter.pubDate).valueOf() -
-      new Date(a.frontmatter.pubDate).valueOf()
-  );
-}
+    .filter((post) => !post.frontmatter.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.frontmatter.pubDate).valueOf() -
+        new Date(a.frontmatter.pubDate).valueOf()
+    );
+};
 
 export const makeUrl = (urlParam, Astro) => {
-  let url = urlParam !== undefined ? "" + urlParam : '';
-  let site = Astro.request.url.includes("localhost") ? Astro.request.url : Astro.site;
+  let url = urlParam !== undefined ? "" + urlParam : "";
+  let site = Astro.request.url.includes("localhost")
+    ? Astro.request.url
+    : Astro.site;
   return new URL(url, site);
-}
+};
+
+export const doSomething = () => {
+  console.log("doSomething");
+};
