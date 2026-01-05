33d48e6a6b7864b4f40135c1a6c1b168e2b81529
jorbencas <jorgebeneytocastello@gmail.com>
2026-01-05 18:45:50 +0100

    Corregir errores tipográficos en el RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,8 @@
 import rss from '@astrojs/rss';
 
 export const get = () => rss({
-  title: 'Problesmas de un desarollador Web',
-  description: 'Blog con articulos sobre programación y ejercicios',
+  title: 'Problemas de un desarrollador Web',
+  description: 'Blog con artículos sobre programación y ejercicios',
   site: import.meta.env.SITE,
   items: import.meta.glob("./posts/*.md"),
 });
