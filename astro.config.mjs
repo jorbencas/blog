3aa086c5aaae7d5cffe2c2faf6d322f36bd36422
jorbencas <jorgebeneytocastello@gmail.com>
2026-01-06 17:11:42 +0100

    Añadido plugin para mostrar el tiempo que se tarda en leer lo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://blog-jorbencas.vercel.app/',
+  site: "https://blog-jorbencas.vercel.app/",
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
