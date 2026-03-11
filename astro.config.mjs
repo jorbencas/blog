841fd8710c59d0bac274414638dd8d1a7b230f4d
jorbencas <jorbencas@gmail.com>
2026-03-11 21:11:04 +0100

    test 1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   output: "static", // <-- Asegúrate de que sea static o borra la línea
   integrations: [tailwind(), mdx(), sitemap()],
