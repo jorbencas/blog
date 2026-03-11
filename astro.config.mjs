d217284932ed6cd3edc65cc13db3157d67d36c3e
jorbencas <jorbencas@gmail.com>
2026-03-11 22:16:11 +0100

    test 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+//import vercel from '@astrojs/vercel'
 export default defineConfig({
   output: "static", // <-- Asegúrate de que sea static o borra la línea
+  // adapter: vercel(),
+  build: {
+    format: "directory", // Fuerza a crear carpetas con index.html
+    outDir: "./dist", // Fuerza a crear la carpeta dist en la raíz
+  },
   integrations: [tailwind(), mdx(), sitemap()],
   site: "https://blog-jorbencas.vercel.app/",
   markdown: {
