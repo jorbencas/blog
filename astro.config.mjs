21c3e3cdd259617d587701b5415b487409f4f83a
Jorge Beneyto Castelló <jorbencas@gmail.com>
2026-03-12 11:32:49 +0100

    Comment out remarkReadingTime import
    Comment out the import statement for remarkReadingTime.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import { remarkReadingTime } from "./remark-reading-time.mjs";
+//import { remarkReadingTime } from "./remark-reading-time.mjs";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
