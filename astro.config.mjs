0d30756ecabb095ddfa42f21404befd957a5b6d6
Jorge Beneyto Castelló <jorbencas@gmail.com>
2026-03-12 11:30:14 +0100

    test sin el remark

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,10 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   // adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap()],
-  site: "https://blog-jorbencas.vercel.app/",
+  site: "https://blog-jorbencas.vercel.app/"/*,
   markdown: {
     remarkPlugins: [remarkReadingTime],
     smartypants: true, // Mejora tipografía (comillas, guiones)
     gfm: true, // Habilita GitHub Flavored Markdown (por defecto es true)
-  },
+  },*/
 });
