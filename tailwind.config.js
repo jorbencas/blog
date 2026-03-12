32861655bf54f25656117e6bcb52b30c902d7f9c
Jorge Beneyto Castelló <jorbencas@gmail.com>
2026-03-12 13:25:13 +0100

    change syntax to ES6 config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import typography from '@tailwindcss/typography';
 import forms from '@tailwindcss/forms';
 import aspectRatio from '@tailwindcss/aspect-ratio';
 
-module.exports = {
+export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
