a08207c82d395dae70089c26383bfdc0a91d235e
Jorge Beneyto Castelló <jorbencas@gmail.com>
2026-03-13 09:21:33 +0100

    simmplificando build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+//import mdx from "@astrojs/mdx";
+//import sitemap from "@astrojs/sitemap";
 //import vercel from '@astrojs/vercel'
 export default defineConfig({
   // adapter: vercel(),
-  integrations: [tailwind(), mdx(), sitemap()],
+  //integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind()],
   site: "https://blog-jorbencas.vercel.app/",
   markdown: {
     remarkPlugins: [remarkReadingTime],
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+//import typography from "@tailwindcss/typography";
+//import forms from "@tailwindcss/forms";
+//import aspectRatio from "@tailwindcss/aspect-ratio";
+
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  //plugins: [typography, forms, aspectRatio],
+    plugins: [],
+};
