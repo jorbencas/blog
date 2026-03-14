8e9565339ef8d670b2de0498fd5c1adaafffbcc1
jorbencas <jorbencas@gmail.com>
2026-03-14 21:53:28 +0100

    fixing propertis metadata of mdx file

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./content/posts" }),
+  loader: glob({ pattern: '**/[^_]*.(md|mdx)', base: "./content/posts" }),
   schema: z.object({
     draft: z.boolean().optional().default(false),
     title: z.string(),
