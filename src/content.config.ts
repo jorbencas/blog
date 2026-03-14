bfde732b3866bf8e108f2b00144292698556c372
jorbencas <jorbencas@gmail.com>
2026-03-14 23:04:43 +0100

    change folder auto-news

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,4 +17,18 @@ const posts = defineCollection({
   }),
 });
 
-export const collections = { posts };
+const autoNews = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.(md|mdx)', base: "./content/auto-news" }),
+  schema: z.object({
+    draft: z.boolean().optional().default(false),
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.string().or(z.date()), // Acepta texto o fecha
+    tags: z.array(z.string()).default([]),
+    slug: z.string().optional(),
+    image: z.string().optional(),
+    author: z.string(),
+    layout: z.string().optional(),
+  }),
+});
+export const collections = { posts, autoNews };
