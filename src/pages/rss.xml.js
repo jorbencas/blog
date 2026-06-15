1e96d106f1a88e32c14362d60f957e339efb024f
jorbencas <jorbencas@gmail.com>
2026-06-15 18:41:20 +0200

    delete implementation of audiosrc

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -15,14 +15,6 @@ export async function GET(context) {
       link: new URL(`/posts/${post.id}`, context.site).toString(),
       categories: post.data.tags || [],
     };
-    if (post.data.audioSrc) {
-      item.enclosure = {
-        url: new URL(post.data.audioSrc, context.site).href,
-        type: "audio/mpeg",
-        length: 0, // Las apps de podcast modernos lo calculan al leer el archivo
-      };
-    }
-
     return item;
   });
 
