635c448ba2c782515b089bf70bfa4388007b9745
jorbencas <jorbencas@gmail.com>
2026-07-08 21:09:40 +0200

    [ui] remove WordPress, Blogger, Google Sites social links

--- src/consts.js
+++ src/consts.js
@@ -25,24 +25,6 @@ export const SOCIAL_LINKS = [
     color: "rgba(226,67,41,0.9)",
   },
   {
-    href: "https://jorgeblogbeneyto.wordpress.com/",
-    icon: "/icons/wordpress.svg",
-    label: "wordpress",
-    color: "rgba(33,117,155,0.9)",
-  },
-  {
-    href: "https://cosasdejorget.blogspot.com/",
-    icon: "/icons/blogger.svg",
-    label: "blogger",
-    color: "rgba(251,143,61,0.9)",
-  },
-  {
-    href: "https://sites.google.com/site/saplicacioneswebjorge2smx/",
-    icon: "/icons/googleSites.svg",
-    label: "google",
-    color: "rgba(219,68,55,0.9)",
-  },
-  {
     href: "mailto:jorgebeneytocastello@gmail.com",
     icon: "/icons/email.svg",
     label: "contacto",
